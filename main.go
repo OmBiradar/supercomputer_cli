37fd6579667aa8fd81bba86d4530a898a140656f
OmBiradar <ombiradar04@gmail.com>
2025-02-08 15:41:47 +0530

    Refactor main function to use cmd package for command execution

--- main.go
+++ main.go
@@ -2,10 +2,12 @@ package main
 
 import (
 	"os"
+
+	"github.com/OmBiradar/supercomputer_cli/cmd/cmd"
 )
 
 func main() {
-	err := root.rootCmd.Execute()
+	err := cmd.RootCmd.Execute()
 	if err != nil {
 		os.Exit(1)
 	}
